5a6135eb6d4307f9ae9508779919aba9f4bc44a9
Ade Prianto <adeprianto21@gmail.com>
2022-03-11 14:36:59 +0700

    style: update ui in dashboard

--- app/src/main/java/id/go/jabarprov/dbmpr/surveisapulubang/presentation/splash_screen/SplashScreenFragment.kt
+++ app/src/main/java/id/go/jabarprov/dbmpr/surveisapulubang/presentation/splash_screen/SplashScreenFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import id.go.jabarprov.dbmpr.surveisapulubang.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.delay
 
 class SplashScreenFragment : Fragment() {
 
@@ -18,5 +21,13 @@ class SplashScreenFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewLifecycleOwner.lifecycleScope.launchWhenResumed {
+            delay(1000)
+            val action = SplashScreenFragmentDirections.actionSplashScreenFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
     }
 }
